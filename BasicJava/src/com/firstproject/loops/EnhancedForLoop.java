1982045cb423b1815e2a7878f7d824efa15c5c61
Sahil Sheikh <sahilsheikh.dev@gmail.com>
2024-10-31 08:13:14 +0530

    array-and-method-added

--- BasicJava/src/com/firstproject/loops/EnhancedForLoop.java
+++ BasicJava/src/com/firstproject/loops/EnhancedForLoop.java
@@ -1,11 +1,15 @@
 package com.firstproject.loops;
 
 public class EnhancedForLoop {
-	
+
 	public static void main(String[] args) {
-		
-		
-		
+
+		int[] arr1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+		for (int temp : arr1) {
+			System.out.print(temp + ", ");
+		}
+
 	}
 
 }
