fc7e42efb8759d6c90f229498f6a090203407cdd
Sahil Sheikh <sahilsheikh.dev@gmail.com>
2024-11-14 08:36:40 +0530

    hibernate save project

--- hibernate/MyHibernate/src/main/java/com/myhibernate/App.java
+++ hibernate/MyHibernate/src/main/java/com/myhibernate/App.java
@@ -1,11 +1,33 @@
 package com.myhibernate;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hibernate Application Started");
+
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+
+		SessionFactory sf = cfg.buildSessionFactory();
+
+		Employee employee1 = new Employee();
+		employee1.setEmpName("Sahil");
+		employee1.setSalary(100000);
+		employee1.setEmpPosition("Intern");
+
+		Session session = sf.openSession();
+		session.beginTransaction();
+		session.save(employee1);
+		session.getTransaction().commit();
+		session.close();
+
+		System.out.println("Hibernate Application Stopped");
 	}
 }
---
+++ hibernate/MyHibernate/src/main/java/com/myhibernate/Employee.java
@@ -0,0 +1,50 @@
+package com.myhibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int empId;
+	private String empName;
+	private int salary;
+	private String empPosition;
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public String getEmpPosition() {
+		return empPosition;
+	}
+
+	public void setEmpPosition(String empPosition) {
+		this.empPosition = empPosition;
+	}
+
+}
