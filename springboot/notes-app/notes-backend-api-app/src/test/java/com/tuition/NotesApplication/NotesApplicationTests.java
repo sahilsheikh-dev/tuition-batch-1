3cec73e04e6ec5db3118b1f57846344bdd1705c4
Sahil Sheikh <sahilsheikh.dev@gmail.com>
2025-01-12 17:04:33 +0530

    notes app init

---
+++ springboot/notes-app/notes-backend-api-app/src/test/java/com/tuition/NotesApplication/NotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuition.NotesApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
